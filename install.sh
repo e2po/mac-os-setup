4dbb9055710c7475286d9c64c7f265c4dc508144
Elvis <elvisporebski@gmail.com>
2024-10-14 02:55:12 +0100

    chore: add CI and test Homebrew installation (#1)
    chore: add CI and test Homebrew installation
    
    ### Changes
    
    - Add CI setup for testing
    - Fix Homebrew installation issues
    - Rename `brew.sh` to `homebrew.sh`

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Install Brew
-source ~/.local/share/mac-os-setup/install/brew.sh
+# Install Homebrew
+source ~/.local/share/mac-os-setup/install/homebrew.sh
