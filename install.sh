7703acc5721b12c8dad548e81313277ba204ea10
e2po <elvisporebski@gmail.com>
2024-10-14 02:16:30 +0100

    feat: install brew

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
+
+# Install Brew
+source ~/.local/share/mac-os-setup/install/brew.sh
---
+++ install/brew.sh
@@ -0,0 +1,4 @@
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
