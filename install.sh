4323df2749c8264d878635878afb748afff0ac64
e2po <elvisporebski@gmail.com>
2024-10-15 01:42:24 +0100

    feat(install): Jetbrains Mono fonts

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Install Jetbrains Mono fonts
+source ~/.local/share/mac-os-setup/install/jetbrains-mono-fonts.sh
+
 # Install Homebrew
 source ~/.local/share/mac-os-setup/install/homebrew.sh
 
---
+++ install/jetbrains-mono-fonts.sh
@@ -0,0 +1,4 @@
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+cp -rf ~/.local/share/mac-os-setup/fonts/* ~/Library/Fonts/
